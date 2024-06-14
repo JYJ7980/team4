a3997be94607a1326e414861601a9008edeb05c6
DESKTOP-IMCRS0E\midu2 <miaa9999>
2024-06-14 14:01:05 +0900

    Revert "#main connection"
    This reverts commit e4b0ef4825162e756611343ad13f28c196b7ff8f.

--- src/kcg/system/t4_main_mng/ctl/T4MainMngCtl.java
+++ src/kcg/system/t4_main_mng/ctl/T4MainMngCtl.java
@@ -2,33 +2,20 @@ package kcg.system.t4_main_mng.ctl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-
 import kcg.common.svc.CommonSvc;
+import kcg.system.t4_cust_mng.svc.T4CustMngSvc;
 import kcg.system.t4_main_mng.svc.T4MainMngSvc;
 
-
 @Controller
-@RequestMapping("system/team4")
+@RequestMapping("t4_main_mng")
 public class T4MainMngCtl {
-
+	
+	@Autowired
+	T4MainMngSvc mainMngSvc;
+	
 	@Autowired
 	CommonSvc commonSvc;
-	
-	@Autowired
-	T4MainMngSvc t4MainMngSvc;
-	
-	@GetMapping("/main")
-	public String t4Main() {
-		return "kcg/system/team4_main_mng/team4_main";
-	}
-	
-	@GetMapping("/notice")
-	public String t4Notice() {
-		return "kcg/system/team4_notice_mng/team4_notice";
-	}
 
 }
