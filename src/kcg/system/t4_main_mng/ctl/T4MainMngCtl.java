e3d43b161a285bc35c1eb11f27d81b454232c618
JYJ7980 <hide7980@naver.com>
2024-06-14 12:12:31 +0900

    #1 main패키지 샘플

---
+++ src/kcg/system/t4_main_mng/ctl/T4MainMngCtl.java
@@ -0,0 +1,21 @@
+package kcg.system.t4_main_mng.ctl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kcg.common.svc.CommonSvc;
+import kcg.system.t4_cust_mng.svc.T4CustMngSvc;
+import kcg.system.t4_main_mng.svc.T4MainMngSvc;
+
+@Controller
+@RequestMapping("t4_main_mng")
+public class T4MainMngCtl {
+	
+	@Autowired
+	T4MainMngSvc mainMngSvc;
+	
+	@Autowired
+	CommonSvc commonSvc;
+
+}
