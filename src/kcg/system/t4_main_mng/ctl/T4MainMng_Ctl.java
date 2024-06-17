15962c1d24c3375abe763b1a5dd289cb76893151
DESKTOP-IMCRS0E\midu2 <miaa9999>
2024-06-17 12:11:06 +0900

    #notice 기본 crud 완성 . 수정폼은 팝업 형식입니다.

--- src/kcg/system/t4_main_mng/ctl/T4MainMng_Ctl.java
+++ src/kcg/system/t4_main_mng/ctl/T4MainMng_Ctl.java
@@ -1,16 +1,21 @@
 package kcg.system.t4_main_mng.ctl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import common.utils.common.CmmnMap;
 import kcg.common.svc.CommonSvc;
 import kcg.system.t4_main_mng.svc.T4MainMngSvc;
+import kcg.system.t4_notice_mng.svc.T4NoticeMng_Svc;
 
 @Controller
 @RequestMapping("system/team4")
-public class T4MainMngCtl {
+public class T4MainMng_Ctl {
 	
 	@Autowired
 	T4MainMngSvc mainMngSvc;
@@ -18,14 +23,14 @@ public class T4MainMngCtl {
 	@Autowired
 	CommonSvc commonSvc;
 	
+	@Autowired
+	T4NoticeMng_Svc t4NoticeMng_Svc;
+	
 	@GetMapping("/main")
 	public String team4Main() {
 		return "kcg/system/team4_mng/main";
 	}
 	
-	@GetMapping("notice_main")
-	public String noticeMain() {
-		return "kcg/system/team4_mng/notice/notice_main";
-	}
+
 
 }
